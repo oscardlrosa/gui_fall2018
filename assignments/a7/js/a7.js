115d21fc3c10b36644c09fc60cf2f0f294ba3ac1
oscardlrosa <Oscar_DeLaRosa@student.uml.edu>
2018-12-16 11:50:49 -0500

    Update to A7
    Fixed and resumitted

--- assignments/a7/js/a7.js
+++ assignments/a7/js/a7.js
@@ -1,26 +1,37 @@
+/*
+Oscar De La Rosa
+JS file for A7
+Oscar_DeLaRosa@student.uml.edu
+
+
+A lot of of thanks to Dr. Heines's sample for the main functionality: https://jesseheines.com/~heines/91.461/91.461-2012-13f/461-lecs/code/jmh-table-v7.html
+
+
+*/
+
 function validate() {
-  $('#input').validate({
+  $('#multTable').validate({
     rules: {
       rowStart: {
-        digits: true,
+        number: true,
         min: -10,
         max: 10,
         required: true,
       },
       rowEnd: {
-        digits: true,
+        number: true,
         min: -10,
         max: 10,
         required: true,
       },
       colStart: {
-        digits: true,
+        number: true,
         min: -10,
         max: 10,
         required: true,
       },
       colEnd: {
-        digits: true,
+        number: true,
         min: -10,
         max: 10,
         required: true,
@@ -28,19 +39,27 @@ function validate() {
     },
     messages: {
       rowStart: {
-        digits: 'ERROR Num',
+        number: 'ERROR Num',
+        min: 'ERROR Min',
+        max: 'ERROR Max',
         required: 'ERROR Req',
       },
       rowEnd: {
-        digits: 'ERROR Num',
+        number: 'ERROR Num',
+        min: 'ERROR Min',
+        max: 'ERROR Max',
         required: 'ERROR Req',
       },
       colStart: {
-        digits: 'ERROR Num',
+        number: 'ERROR Num',
+        min: 'ERROR Min',
+        max: 'ERROR Max',
         required: 'ERROR Req',
       },
       colEnd: {
-        digits: 'ERROR Num',
+        number: 'ERROR Num',
+        min: 'ERROR Min',
+        max: 'ERROR Max',
         required: 'ERROR Req',
       },
     },
@@ -59,65 +78,96 @@ function validate() {
     errorPlacement: function (error, element) {
       error.insertAfter(element);
     },
+
+    onkeyup: function (element, event) {
+      autoSubmit();
+    },
   });
 }
 
 function makeTable() {
-  var resultTable = document.getElementById('ghostTable');
-  if (resultTable != null) resultTable.remove();
+  var rStart = Number(document.getElementById('rowStart').value);
+  var rEnd = Number(document.getElementById('rowEnd').value);
+  var cStart = Number(document.getElementById('colStart').value);
+  var cEnd = Number(document.getElementById('colEnd').value);
 
-  var temp;
-  var rowStart = +document.getElementById('rowStart').value;
-  var rowEnd = +document.getElementById('rowEnd').value;
-  var colStart = +document.getElementById('colStart').value;
-  var colEnd = +document.getElementById('colEnd').value;
+  $('#warningMsg').empty();
 
-  var bRef = document.getElementsByTagName('body')[0];
+  var tempNum;
+  if (rStart > rEnd) {
+    $('#warningMsg').append('<p class="warningCl">Correcting order. Swapping numbers</p>');
 
-  var crtTbl = document.createElement('table'); /*create table*/
-  var crtTblBod = document.createElement('tbody'); /*create table body*/
-
-  for (var rowFirstInt = rowStart, rowLastInt = rowEnd + 1;
-    rowFirstInt <= rowLastInt; rowFirstInt++) {
-
-    var row = document.createElement('tr');
-
-    for (var colFirstInt = colStart, colLastInt = colEnd + 1;
-      colFirstInt <= colLastInt; colFirstInt++) {
-      var cell = document.createElement('td');
-      var cellTxt;
-      var cellStyle = 'padding: 10px; color: #FFFFFF;';
-
-      if (rowFirstInt == rowStart && colFirstInt == colStart) {
-        cellTxt = document.createTextNode('');
-        cell.setAttribute('style', cellStyle + 'background-color: #FFFFFF');
-      } else if (rowFirstInt == rowStart) {
-        cellTxt = document.createTextNode(colFirstInt - 1);
-        cell.setAttribute('style', cellStyle + 'background-color: #000000');
-      } else if (colFirstInt == colStart) {
-        cellTxt = document.createTextNode(rowFirstInt - 1);
-        cell.setAttribute('style', cellStyle + 'background-color: #000000');
-      } else {
-        cellTxt = document.createTextNode((rowFirstInt - 1) * (colFirstInt - 1));
-        cell.setAttribute('style', cellStyle + 'background-color: #0FF0F0');
-      }
-
-      cell.appendChild(cellTxt);
-      row.appendChild(cell);
-    }
-
-    crtTblBod.appendChild(row);
+    tempNum = rStart;
+    rStart = rEnd;
+    rEnd = tempNum;
   }
 
-  crtTbl.appendChild(crtTblBod);
-  bRef.appendChild(crtTbl);
-  crtTbl.setAttribute('id', 'ghostTable');
+  if (cStart > cEnd) {
+    $('#warningMsg').append('<p class="warningCl">Correcting order. Swapping numbers</p>');
+
+    tempNum = cStart;
+    cStart = cEnd;
+    cEnd = tempNum;
+  }
+
+  var mat = {};
+
+  var rows = Math.abs(rEnd - rStart);
+  var cols = Math.abs(cEnd - cStart);
+
+  var rIndex = rStart;
+  var cIndex = cStart;
+
+  for (var x = 0; x <= cols; x++) {
+    var tempArr = [];
+
+    for (var y = 0; y <= rows; y++) {
+      var product = rIndex * cIndex;
+      tempArr[y] = product;
+      rIndex++;
+    }
+
+    mat['row' + x] = tempArr;
+
+    rIndex = rStart;
+    cIndex++;
+  }
+
+  var content = '';
+  content += '<table>';
+  content += '<tr><td></td>';
+
+  for (var f = rStart; f <= rEnd; f++) {
+    content += '<td>' + f + '</td>';
+  }
+
+  content += '</tr>';
+
+  cIndex = cStart;
+
+  for (var g = 0; g <= cols; g++) {
+    content += '<tr><td>' + cIndex + '</td>';
+
+    for (var h = 0; h <= rows; h++) {
+      content += '<td>' + mat['row' + g][h] + '</td>';
+    }
+
+    cIndex++;
+
+    content += '</tr>';
+  }
+
+  content += '</table>';
+
+  $('#ghostTable').html(content);
+
+  return false;
 }
 
-function clearForm() {
-  document.getElementById('input').reset();
-}
-
-$('#destroy').click(function () {
-  $('#ghostTable div').hide();
-});
+// function clearForm() {
+//   $('#multTable').reset();
+//
+//   $('#destroy').click(function () {
+//     $('#ghostTable div').hide();
+//   });
+// }
